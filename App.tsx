7e346fe94ab67e2bf8415c290cbae90272ed70f6
Paulo Augusto <paulo-augusto12@outlook.com>
2023-03-20 08:47:14 -0300

    feat: added navigation container to app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import { Home } from "./src/screens/Home";
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 export default function App() {
+  const Navigation = createNativeStackNavigator();
   return (
     <>
-      <StatusBar style="light" />
-      <Home />
+      <NavigationContainer>
+        <StatusBar style="light" />
+        <Navigation.Navigator screenOptions={{ headerShown: false }}>
+          <Navigation.Screen name="homePage" component={Home} />
+        </Navigation.Navigator>
+      </NavigationContainer>
     </>
   );
 }
