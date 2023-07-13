c654cb324d210cff8f4f7edc04403e3febc3f5fd
Paulo Augusto <paulo-augusto12@outlook.com>
2023-07-13 13:15:13 -0300

    chore: developed dependency injection for get game data use case

--- src/di/index.ts
+++ src/di/index.ts
@@ -1,5 +1,6 @@
 import { GamesRepository } from "../data/repositories/gamesRepository/GamesRepository";
 import { GetAllGamesUseCase } from "../domain/useCases/GamesUseCases/GetAllGamesUseCase";
+import { GetGameDataUseCase } from "../domain/useCases/GamesUseCases/GetGameDataUseCase";
 import { HttpService } from "../services/http/HttpService";
 
 const HTTP = new HttpService();
@@ -10,7 +11,7 @@ const repositories = {
 
 const GAMES_USE_CASES = {
   getAllGames: new GetAllGamesUseCase(repositories.GAMES),
+  getGameData: new GetGameDataUseCase(repositories.GAMES),
 };
-
 
 export { GAMES_USE_CASES };
