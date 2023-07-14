82e16b63e0db6167d4853d21832c68df0b57c6a9
Paulo Augusto <paulo-augusto12@outlook.com>
2023-07-14 12:55:57 -0300

    fix: added key missing prop

--- src/screens/About/components/Links/index.tsx
+++ src/screens/About/components/Links/index.tsx
@@ -12,11 +12,12 @@ interface ILinkProps {
 export function Links({ links }: ILinkProps) {
   return (
     <View style={{ flexDirection: "column", gap: 18, width: "100%" }}>
-      {links.map(({ icon, onPress, text }) => (
+      {links.map(({ icon, onPress, text }, index) => (
         <TouchableOpacity
           onPress={() => {
             onPress();
           }}
+          key={index}
         >
           <ExternalLinkCard title={text} icon={icon} />
         </TouchableOpacity>
