f96077d5ce1a728bdc00c5061fdd2a33cc4e58db
Paulo Augusto <paulo-augusto12@outlook.com>
2023-07-14 12:59:12 -0300

    refactor: isolated screen header in a dedicated component

---
+++ src/screens/About/components/GameDataHeader/index.tsx
@@ -0,0 +1,61 @@
+import { View, Image, Text } from "react-native";
+
+interface IGameDataHeaderProps {
+  thumbnail: string;
+  name: string;
+  shortDescription: string;
+}
+
+export function GameDataHeader({
+  name,
+  shortDescription,
+  thumbnail,
+}: IGameDataHeaderProps) {
+  return (
+    <View
+      style={{
+        alignItems: "center",
+        borderRadius: 8,
+        width: "100%",
+        height: 250,
+      }}
+    >
+      <Image
+        source={{ uri: thumbnail }}
+        style={{
+          width: "100%",
+          height: 250,
+          opacity: 0.5,
+          borderRadius: 8,
+        }}
+      />
+      <View
+        style={{
+          position: "absolute",
+          bottom: 60,
+          flexDirection: "column",
+          justifyContent: "space-between",
+          padding: 6,
+        }}
+      >
+        <Text
+          style={{
+            color: "white",
+            fontSize: 32,
+            fontWeight: "bold",
+          }}
+        >
+          {name}
+        </Text>
+        <Text
+          style={{
+            color: "white",
+            fontSize: 16,
+          }}
+        >
+          {shortDescription}
+        </Text>
+      </View>
+    </View>
+  );
+}
--- src/screens/About/index.tsx
+++ src/screens/About/index.tsx
@@ -1,25 +1,17 @@
 import React, { useEffect } from "react";
 import { AboutGameProps } from "../../routes/stack.routes";
 
-import {
-  View,
-  FlatList,
-  ActivityIndicator,
-  TouchableOpacity,
-  Text,
-  Image,
-} from "react-native";
+import { View, ActivityIndicator, TouchableOpacity } from "react-native";
 
 // Components
 
-import { GameCard } from "../../components/gameCard";
-import { ExternalLinkCard, Links } from "./components";
+import { GameDataHeader, Links } from "./components";
 
 //
 
 // Icons
 
-import { ArrowLeft, LinkSimple, UsersThree } from "phosphor-react-native";
+import { ArrowLeft } from "phosphor-react-native";
 
 //
 
@@ -46,51 +38,11 @@ export function About({ route }: AboutGameProps) {
         <ActivityIndicator />
       ) : (
         <View style={{ alignItems: "center", gap: 48 }}>
-          <View
-            style={{
-              alignItems: "center",
-              borderRadius: 8,
-              width: "100%",
-              height: 250,
-            }}
-          >
-            <Image
-              source={{ uri: selectedGame.thumbnail }}
-              style={{
-                width: "100%",
-                height: 250,
-                opacity: 0.5,
-                borderRadius: 8,
-              }}
-            />
-            <View
-              style={{
-                position: "absolute",
-                bottom: 60,
-                flexDirection: "column",
-                justifyContent: "space-between",
-                padding: 6,
-              }}
-            >
-              <Text
-                style={{
-                  color: "white",
-                  fontSize: 32,
-                  fontWeight: "bold",
-                }}
-              >
-                {name}
-              </Text>
-              <Text
-                style={{
-                  color: "white",
-                  fontSize: 16,
-                }}
-              >
-                {selectedGame.shortDescription}
-              </Text>
-            </View>
-          </View>
+          <GameDataHeader
+            name={selectedGame.title}
+            thumbnail={selectedGame.thumbnail}
+            shortDescription={selectedGame.shortDescription}
+          />
           <Links links={elements.links} />
         </View>
       )}
