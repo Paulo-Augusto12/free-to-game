9ea86d45edfe6a92f2052f12c42b75593303b4e4
Paulo <paulo-augusto12@outlook.com>
2023-07-15 21:02:11 -0300

    chore: developed dependenc yinjeciton for get games by genre use case

--- src/di/index.ts
+++ src/di/index.ts
@@ -1,6 +1,7 @@
 import { GamesRepository } from "../data/repositories/gamesRepository/GamesRepository";
 import { GetAllGamesUseCase } from "../domain/useCases/GamesUseCases/GetAllGamesUseCase";
 import { GetGameDataUseCase } from "../domain/useCases/GamesUseCases/GetGameDataUseCase";
+import { GetGamesByGenreUseCase } from "../domain/useCases/GamesUseCases/GetGamesByGenreUseCase";
 import { HttpService } from "../services/http/HttpService";
 
 const HTTP = new HttpService();
@@ -12,6 +13,7 @@ const repositories = {
 const GAMES_USE_CASES = {
   getAllGames: new GetAllGamesUseCase(repositories.GAMES),
   getGameData: new GetGameDataUseCase(repositories.GAMES),
+  getGamesByGenre: new GetGamesByGenreUseCase(repositories.GAMES),
 };
 
 export { GAMES_USE_CASES };
